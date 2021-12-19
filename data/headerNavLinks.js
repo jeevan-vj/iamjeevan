9becd1ab1e024da5e77a2edb7e8db2708c04236c
jeevan wijerathna <jeevanwijerathna@gmail.com>
2021-12-19 21:11:53 +0800

    resume page added

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  // { href: '/projects', title: 'Projects' },
+  // { href: '/resume', title: 'Resume' },
   { href: '/about', title: 'About' },
 ]
 
---
+++ pages/resume.js
@@ -0,0 +1,7 @@
+export default function Resume() {
+  return (
+    <>
+      <p>Resume page - in progress</p>
+    </>
+  )
+}
