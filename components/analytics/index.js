cfb7c43b3d311645a1e67484bd72b872342b09e2
jeevan-vj <jeevan90wijerathna@gmail.com>
2025-07-25 00:41:25 +1200

    Add Vercel Analytics integration and update dependencies
    - Introduced the Vercel Analytics component to the analytics section for enhanced tracking capabilities.
    - Updated package.json to include the new dependency "@vercel/analytics" for analytics integration.
    
    These changes aim to improve the project's analytics functionality and provide better insights into user interactions.

--- components/analytics/index.js
+++ components/analytics/index.js
@@ -2,6 +2,7 @@ import GA from './GoogleAnalytics'
 import Plausible from './Plausible'
 import SimpleAnalytics from './SimpleAnalytics'
 import siteMetadata from '@/data/siteMetadata'
+import { Analytics as VercelAnalytics } from "@vercel/analytics/next"
 
 const isProduction = process.env.NODE_ENV === 'production'
 
@@ -11,6 +12,7 @@ const Analytics = () => {
       {isProduction && siteMetadata.analytics.plausibleDataDomain && <Plausible />}
       {isProduction && siteMetadata.analytics.simpleAnalytics && <SimpleAnalytics />}
       {isProduction && siteMetadata.analytics.googleAnalyticsId && <GA />}
+      {isProduction && <VercelAnalytics />}
     </>
   )
 }
